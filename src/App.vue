0f4435fe139c939412580da6f77b71ab5dd83d63
Luis Eduardo Ocegueda Cortes <luiseduardooceguedacortes@Luiss-MacBook-Air.local>
2025-01-13 17:28:51 -0600

    feat:  add about me section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import MainLayout from "./layouts/MainLayout.vue";
 import Header from "./components/Header.vue";
+import AboutMe from "./components/AboutMe.vue";
 </script>
 
 <template>
   <MainLayout>
     <Header />
+    <main class="mt-16">
+      <AboutMe />
+    </main>
   </MainLayout>
 </template>
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="space-y-3">
+    <h2 class="text-xl font-semibold">Sobre Mí</h2>
+    <p class="jetbrains-mono text-md text-neutral-600">
+      Programador autodidacta con más de 2 años de experiencia en el mundo del
+      desarrollo web, me apasiona aprender cosas nuevas, algo que me caracteriza
+      es la responsabilidad que tomo en todos los proyectos que realizo, no solo
+      de escribir código, si no de también tener una visión crítica del
+      producto, las funcionalidades y el negocio.
+    </p>
+  </section>
+</template>

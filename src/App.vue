f7fb12b6370d73e4c8ad55b07ee075aca2809804
Luis Eduardo Ocegueda Cortes <luiseduardooceguedacortes@Luiss-MacBook-Air.local>
2025-01-13 22:39:33 -0600

    feat:  add vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,11 @@ import Skills from "./components/Skills.vue";
 import Projects from "./components/Projects.vue";
 import Certificates from "./components/Certificates.vue";
 import Footer from "./components/Footer.vue";
+import { Analytics } from "@vercel/analytics/vue";
 </script>
 
 <template>
+  <Analytics />
   <MainLayout>
     <Header />
     <main class="my-16 space-y-10">
